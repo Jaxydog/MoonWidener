2d1da99954a9bc433b75a5221bef4b5d1c284bcd
jaxydog <32988901+jaxydog@users.noreply.github.com>
2023-01-22 19:11:51 -0500

    Set up mod

---
+++ src/main/java/dev/jaxydog/widemoon/MoonWidener.java
@@ -0,0 +1,16 @@
+package dev.jaxydog.widemoon;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoonWidener implements ModInitializer {
+
+	public static final String MOD_ID = "widemoon";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MoonWidener.MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		MoonWidener.LOGGER.info("And just like that, the moon is fat!");
+	}
+}
---
+++ src/main/java/dev/jaxydog/widemoon/mixin/WorldRendererMixin.java
@@ -0,0 +1,45 @@
+package dev.jaxydog.widemoon.mixin;
+
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.util.math.Matrix4f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(WorldRenderer.class)
+public class WorldRendererMixin {
+
+	@Unique
+	private Matrix4f originalMatrix;
+
+	@ModifyVariable(
+		method = "Lnet/minecraft/client/render/WorldRenderer;renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/math/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V",
+		at = @At(
+			value = "INVOKE",
+			target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderTexture(ILnet/minecraft/util/Identifier;)V",
+			ordinal = 0
+		),
+		ordinal = 1
+	)
+	private Matrix4f wideMoon$scaleSun(Matrix4f in) {
+		originalMatrix = in.copy();
+		return in.copy();
+	}
+
+	@ModifyVariable(
+		method = "Lnet/minecraft/client/render/WorldRenderer;renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/math/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V",
+		at = @At(
+			value = "INVOKE",
+			target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderTexture(ILnet/minecraft/util/Identifier;)V",
+			ordinal = 1
+		),
+		ordinal = 1
+	)
+	private Matrix4f wideMoon$scaleMoon(Matrix4f in) {
+		var copy = originalMatrix.copy();
+		copy.multiply(Matrix4f.scale(16.0f, 1.0f, 16.0f));
+		originalMatrix = null;
+		return copy;
+	}
+}
